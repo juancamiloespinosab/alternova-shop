8e4b7c469d76f2e98c169e27ba41b53536ab8d1e
Juan Espinosa <juancamiloespinosab@gmail.com>
2023-03-12 13:18:26 -0500

    refactor(table): Ajustar formato currency y mejorar selector del componente de tabla

--- src/app/components/molecules/table/table.component.ts
+++ src/app/components/molecules/table/table.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CoreModule } from '@core/core.module';
-import { TABLE_COLUMN_FORMAT_VALUE } from '@core/enums';
 import { TableConfig } from '@core/interfaces';
 import { SharedModule } from '@shared/shared.module';
 
 @Component({
   imports: [CoreModule, SharedModule],
-  selector: 'app-table',
+  selector: 'table[app-table]',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
   standalone: true,
--- src/app/core/pipes/table-column-format.pipe.ts
+++ src/app/core/pipes/table-column-format.pipe.ts
@@ -9,7 +9,7 @@ export class TableColumnFormatPipe implements PipeTransform {
   transform(value: string, format: TABLE_COLUMN_FORMAT_VALUE | string) {
     switch (format) {
       case TABLE_COLUMN_FORMAT_VALUE.CURRENCY:
-        return new CurrencyPipe('en').transform(value, 'USD', 'symbol', '4.0');
+        return new CurrencyPipe('en').transform(value, 'USD', 'symbol', '1.0');
     }
 
     return value;
