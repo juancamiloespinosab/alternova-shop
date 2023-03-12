b3b98965219864e4c372c065e880d31bfe60e6ca
Juan Espinosa <juancamiloespinosab@gmail.com>
2023-03-11 21:50:50 -0500

    feat: Agregar funcionalidad de descarga del archivo json con la orden mediante la directiva "download-json"

--- src/app/components/organisms/cart/cart.component.ts
+++ src/app/components/organisms/cart/cart.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CoreModule } from '@core/core.module';
 import { CartStateService } from '@core/services/state/cart-state.service';
 import { SharedModule } from '@shared/shared.module';
 
 @Component({
-  imports: [SharedModule],
+  imports: [CoreModule, SharedModule],
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss'],
--- src/app/core/directives/download-json.directive.ts
+++ src/app/core/directives/download-json.directive.ts
@@ -1,8 +1,25 @@
+import { Input } from '@angular/core';
+import { HostListener } from '@angular/core';
 import { Directive } from '@angular/core';
 
 @Directive({
-  selector: 'a[downloadJson]',
+  selector: 'button[downloadJson]',
 })
 export class DownloadJsonDirective {
   constructor() {}
+
+  @Input() json: any;
+
+  @HostListener('click')
+  onClick() {
+    const dataStr = JSON.stringify(this.json);
+    const dataUri =
+      'data:application/json;charset=utf-8,' + encodeURIComponent(dataStr);
+    const date = new Date().toLocaleString();
+    const exportFileDefaultName = `Order ${date}.json`;
+    const linkElement = document.createElement('a');
+    linkElement.setAttribute('href', dataUri);
+    linkElement.setAttribute('download', exportFileDefaultName);
+    linkElement.click();
+  }
 }
