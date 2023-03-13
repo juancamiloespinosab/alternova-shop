fec171503924aeaa0c178988c689d6a1a4b778fe
Juan Espinosa <juancamiloespinosab@gmail.com>
2023-03-12 19:00:13 -0500

    feat(product): Agregar modulo "NgOptimizedImage" para aplicar lazy loading a las imagenes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MATERIAL_MODULES } from './material.index';
+import { NgOptimizedImage } from '@angular/common';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MATERIAL_MODULES],
-  exports: [CommonModule, MATERIAL_MODULES],
+  imports: [CommonModule, NgOptimizedImage, MATERIAL_MODULES],
+  exports: [CommonModule, NgOptimizedImage, MATERIAL_MODULES],
 })
 export class SharedModule {}
