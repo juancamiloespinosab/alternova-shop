a1d1933d67904774a9a927b81fbb008a4e2f4ae2
Juan Espinosa <juancamiloespinosab@gmail.com>
2023-03-13 10:53:23 -0500

    feat: Crear funcionalidad para eliminar un item especifico del carrito: * Crear metodo "removeProductFromCart" en el Cart Manager * Agregar acciones a la configuración de la tabla * Crear manejador de acciones en el componente Cart

--- src/app/components/organisms/cart/cart.component.ts
+++ src/app/components/organisms/cart/cart.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { TableComponent } from '@components/molecules/table/table.component';
 import { CoreModule } from '@core/core.module';
 import { TABLE_ADAPTER_VALUE, TABLE_COLUMN_FORMAT_VALUE } from '@core/enums';
 import { TableConfig } from '@core/interfaces';
+import { TableAction } from '@core/interfaces/table-action.interface';
 import { CartManagerService } from '@core/services/app/cart-manager.service';
 import { CartStateService } from '@core/services/state/cart-state.service';
 import { SharedModule } from '@shared/shared.module';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   imports: [CoreModule, SharedModule, TableComponent],
@@ -14,7 +16,7 @@ import { SharedModule } from '@shared/shared.module';
   styleUrls: ['./cart.component.scss'],
   standalone: true,
 })
-export class CartComponent {
+export class CartComponent implements OnDestroy {
   TABLE_ADAPTER_VALUE = TABLE_ADAPTER_VALUE;
 
   tableConfig: TableConfig = {
@@ -41,7 +43,16 @@ export class CartComponent {
         format: TABLE_COLUMN_FORMAT_VALUE.CURRENCY,
       },
     ],
+    actions: [
+      {
+        name: 'remove',
+        icon: 'delete',
+        color: 'accent',
+      },
+    ],
   };
+
+  actionaObservableSubscription!: Subscription;
 
   constructor(
     public cartStateService: CartStateService,
@@ -50,5 +61,26 @@ export class CartComponent {
 
   clearCart() {
     this.cartManagerService.clearCart();
+  }
+
+  subscribeToTableActionsObservable(
+    actionaObservable: Observable<TableAction>
+  ) {
+    this.actionaObservableSubscription = actionaObservable.subscribe(
+      (tableAction: TableAction) => this.tableActionsHandler(tableAction)
+    );
+  }
+
+  tableActionsHandler(tableAction: TableAction) {
+    const DISPATCH_CART_ACTION_DEFAULT = null;
+    const DISPATCH_CART_ACTION: { [key: string]: any } = {
+      remove: this.cartManagerService.removeProductFromCart(tableAction.item),
+    };
+    DISPATCH_CART_ACTION[tableAction.actionName] ||
+      DISPATCH_CART_ACTION_DEFAULT;
+  }
+
+  ngOnDestroy(): void {
+    this.actionaObservableSubscription?.unsubscribe();
   }
 }
